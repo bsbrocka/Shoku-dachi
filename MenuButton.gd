5fd795cc1cb2d5c2bda365ff0d851a5be815eb38
bsbrocka <bsbrocka@up.edu.ph>
2021-04-14 11:25:41 +0800

    added functionality for sprint 2

--- MenuButton.gd
+++ MenuButton.gd
@@ -1,18 +1,23 @@
 extends MenuButton
 
+signal collect
 var popup
 
+
 func _ready():
-	$Main.visible = false
-	$Main/HUD.scale = Vector2(0,0)
+	$SunlightMain.visible = false
+	$SunlightMain/SunlightHUD.scale = Vector2(0,0)
 	popup = get_popup()
 	popup.add_item("collect money")
 	popup.add_item("play minigame")
 	popup.connect("id_pressed",self,"_on_item_pressed")
 
 func _on_item_pressed(ID):
+	if popup.get_item_text(ID) == "collect money":
+		emit_signal("collect")
+
 	if popup.get_item_text(ID) == "play minigame":
-		$Main.visible = true
-		$Main/HUD.scale = Vector2(1,1)
-		$Main/HUD/StartButton.show()
-		$Main/HUD/ExitButton.show()
+		$SunlightMain.visible = true
+		$SunlightMain/SunlightHUD.scale = Vector2(1,1)
+		$SunlightMain/SunlightHUD/StartButton.show()
+		$SunlightMain/SunlightHUD/ExitButton.show()
--- Plant.gd
+++ Plant.gd
@@ -1,6 +1,21 @@
 extends Area2D
 
-func _input_event(viewport, event, shape_idx):
+var collect_time = 20
+var currency = 0
+var curr_time
+
+onready var number_label = $Bar/Count/Background/Number
+onready var bar = $Bar/Gauge
+onready var tween = $Tween
+
+func _ready():
+	$AnimatedSprite.play()
+	bar.max_value = collect_time
+	curr_time = collect_time
+	number_label.text = str(curr_time)
+	$CollectTimer.start()
+
+func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_LEFT \
 	and event.is_pressed():
@@ -8,3 +23,25 @@ func _input_event(viewport, event, shape_idx):
 		
 func on_click():
 	print("Click")
+
+func _on_CollectTimer_timeout():
+	curr_time -= 1
+	number_label.text = str(curr_time)
+	if curr_time <= 0:
+		$CollectTimer.stop()
+	bar.value += 1
+
+func collect():
+	if curr_time > 0:
+		pass
+	else:
+		currency += 20
+		$Counter/Background/Number.text = str(currency)
+		timer_reset()
+
+func timer_reset():
+	curr_time = collect_time
+	number_label.text = str(curr_time)
+	bar.value = 0
+	$CollectTimer.start()
+	
