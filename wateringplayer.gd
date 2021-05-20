c63d5103efabd664f7bf245463f57374ef973326
bsbrocka <bsbrocka@up.edu.ph>
2021-05-20 15:28:53 +0800

    changed move_left and move_right to ui_left and ui_right

--- wateringplayer.gd
+++ wateringplayer.gd
@@ -41,7 +41,7 @@ func _physics_process(delta: float)->void:
 func get_direction()->Vector2:
 	#movement to right =1, movement to left=-1, if player presses both keys the player will not move
 	#jump=-1 since y-axis points down
-	return Vector2(Input.get_action_strength("move_right")-Input.get_action_strength("move_left"),
+	return Vector2(Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left"),
 	-1.0 if Input.is_action_just_pressed("ui_up") and is_on_floor() else 1.0)
 
 func calculate_velocity(linear_velocity:Vector2, speed:Vector2,direction:Vector2,is_jump_interrupted:bool) ->Vector2:
